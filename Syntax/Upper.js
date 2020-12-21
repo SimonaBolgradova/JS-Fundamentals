66488e954075da105112f6dea4f0f1e2c73d85f8
Simona Bolgradova <75329832+SimonaBolgradova@users.noreply.github.com>
2020-12-21 18:11:46 +0200

    Create Upper.js

---
+++ Syntax/Upper.js
@@ -0,0 +1,10 @@
+function solve (char1){
+    let char2= char1.toUpperCase();
+
+    if (char1 == char2){
+        console.log("upper-case");
+    }else{
+        console.log("lower-case");
+    }
+}
+solve('L');
