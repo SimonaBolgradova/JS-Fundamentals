3c0a10e945ab1b96bb103b826ec1d7e670ad944d
Simona Bolgradova <75329832+SimonaBolgradova@users.noreply.github.com>
2020-12-21 18:09:32 +0200

    Create TownInfo.js

---
+++ Syntax/TownInfo.js
@@ -0,0 +1,10 @@
+function solve(town, population, area){
+    let town1 = (town);
+    let population1 = parseInt(population);
+    let area1 = parseInt(area);
+    console.log(`Town ${town1} has population of ${population1} and area ${area1} square km.`)
+}
+solve('Sofia',
+1286383,
+492
+);
