db2f0deabca0e76fba660a079cf303c4fac81cbb
Simona Bolgradova <75329832+SimonaBolgradova@users.noreply.github.com>
2020-12-26 22:59:09 +0200

    Create ArrayRotation.js

---
+++ Arrays/ArrayRotation.js
@@ -0,0 +1,8 @@
+function solve (arr,n){
+    for (let i = 0; i< n; i++){
+        let el = arr.shift();
+        arr.push(el);
+    }
+    console.log(arr.join(' '));
+}
+solve([51, 47, 32, 61, 21], 2)
