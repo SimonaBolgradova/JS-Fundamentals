1b5a7a5b9f52523dc4dff2865e3a6be3db9bed36
Simona Bolgradova <75329832+SimonaBolgradova@users.noreply.github.com>
2020-12-29 19:15:46 +0200

    Delete ListOfProductss.js

--- ListOfProductss.js
+++
@@ -1,8 +0,0 @@
-function solve(input){
-    let sorted = input.sort((a,b)=>a.localeCompare(b));
-    for (let i = 0; i < sorted.length; i++) {
-        const element = sorted[i];
-        console.log(`${i+1}.${element}`);
-    }
-}
-solve(["Potatoes", "Tomatoes", "Onions", "Apples"])
