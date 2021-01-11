85c31278f4bcf737dc1709c46d7ac33c6caec1cf
Simona Bolgradova <75329832+SimonaBolgradova@users.noreply.github.com>
2021-01-11 18:06:19 +0200

    Create CutAndReverse.js

---
+++ Text Processing/CutAndReverse.js
@@ -0,0 +1,7 @@
+function solve(input){
+ let firstHalf = input.substring(0, input.length/2);
+ let secod = input.substring(input.length/2);
+ console.log(firstHalf.split('').reverse().join(''));
+ console.log(secod.split('').reverse().join(''))
+}
+solve('sihToDtnaCuoYteBIboJsihTtAdooGoSmI')
